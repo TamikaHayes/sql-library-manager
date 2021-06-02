7cb2c31533384b8c08ceb7a0b1f2214a4efe647d
TamikaHayes <tamika.hayes@gmail.com>
2021-06-01 21:20:42 -0700

    Updated routes/index.js with 500 server error
    --Also, in app.js, added {err} object as second parameter for render() method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      //Forward error to the global error handler
+      next(error);
+      //res.status(500).send(error);
+    }
+  }
+}
+
+
+/*GET generated error route - create and throw 500 error*/
+router.get('/error', (req, res, next) => {
+  
+  const err = new Error();
+  err.status = 500;
+  err.message = `Uh-oh. Looks like trouble with the server. Status: ${err.status}`;
+  console.log(err.status);
+  console.log(err.message);
+  //res.render('error', { err } );
+  throw err;
+});
+
+/* GET project pages dynamically, based on id property. */
+// router.get('/projects/:id', function(req, res, next) {
+//   const projectId = req.params.id;
+//   const project = projects.find( ({ id }) => id === +projectId );
+  
+//   //check to see if requested project page exists
+//   if (project) {
+//     // if true, pass the project data to the 'project' pug template
+//     res.render('project', { project });
+//   } else {
+//     const err = new Error();
+//     err.status = 404;
+//     err.message = `Looks like the page you requested doesn't exist. Status: ${err.status}`;
+//     next(err);
+    
+//   }
+// });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  console.log(books);
+  throw new Error;
+  //res.json(books);
+
+  //res.render('index', { /books, title: 'Express' });
+}));
 
 module.exports = router;
